7918349899b554eda80ecdd1f4be4ee4eca57d98
Clayton Passos <netstart@gmail.com>
2019-04-05 14:59:48 -0300

    entity

---
+++ company/src/main/kotlin/ifood/company/entity/Company.kt
@@ -0,0 +1,23 @@
+package ifood.company.entity
+
+import javax.persistence.Entity
+import javax.persistence.Id
+
+@Entity
+data class Company(@Id val id: Long,
+                   val responsableName: String, val position: String, val phone: String, val email: String,
+                   val companyName: String, val segment: Segment, val employerTotal: EmployerTotal? = null)
+
+enum class Segment {
+    CONTABILIDADE,
+    ARMARINHOS,
+    LAVANDERIA,
+    CINEMA,
+    RESTAURANTE
+}
+
+enum class EmployerTotal(val value: Int) {
+    FIVE(5),
+    TEM(10),
+    FIFTY(50)
+}
---
+++ company/src/main/kotlin/ifood/company/repository/CompanyRepository.kt
@@ -0,0 +1,2 @@
+package ifood.company.repository
+
