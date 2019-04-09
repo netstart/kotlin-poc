02e54a36743946a1afa2865a34552f581b84adff
Clayton Passos <netstart@gmail.com>
2019-04-09 15:46:45 -0300

    controle de fluxo when

--- company/src/main/kotlin/ifood/company/controller/language/ControleFluxoWhen.kt
+++ company/src/main/kotlin/ifood/company/controller/language/ControleFluxoWhen.kt
@@ -2,12 +2,17 @@ package ifood.company.controller.language
 
 // Máquina de café
 fun main(args: Array<String>) {
-    val total = 1
+    val total = 2
 
     when (total) {
         1 -> println("Café puro curto")
-        2 -> println("Café puro longo")
-        3 -> println("Café com leite")
+        2 -> {
+            println("Café puro longo")
+            println("e com açucar")
+        }
+        3, 4 -> println("Café com leite")
+        5 -> println("Café com chocolate")
+
         else -> println("Nenhuma opção selecionada")
     }
 
